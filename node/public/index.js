9257f5fd1b935c478eeb0cc9edfd02d19130df37
eugene <uwm1004@gmail.com>
2023-02-16 20:23:08 +0900

    feat: add jquery

--- node/public/index.js
+++ node/public/index.js
@@ -4,15 +4,20 @@ import "./style.css"
 import land from "./images/land.jpg"
 import sheep from "./images/117.png"
 import burger from "./images/hamburger.svg"
+import './normalize.css'
+// import $ from './jquery-3.6.0.min.js'
 
 const text = hello('hello')
 const num = add(1,2)
 const img = `<img src="${land}" style="width:100px; height:auto">`
 const png = `<img src="${sheep}" style="width:100px; height:auto">`
 const svg = `<img src="${burger}" style="width:100px; height:auto">`
-document.getElementById('root').innerHTML = `
-<p>${img}</p>
-<p>${svg}</p>
-${text} webpack nodejs!!!${num}
-<p>${png}</p>
-`
+
+$('document').ready(function(){
+    document.getElementById('root').innerHTML = `
+    <p>${img}</p>
+    <p>${svg}</p>
+    ${text} webpack nodejs!!!${num}
+    <p>${png}</p>
+    `    
+});
